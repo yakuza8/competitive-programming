1f509a32d4073afd1b93aa7decf22801ed0dc546
Berat Cankar <berat.cankar@gmail.com>
2023-01-01 12:25:34 +0000

    Add some sql solutions. Oh shit my global git config f*cked up everything

---
+++ sql/group_sold_products_by_the_date.sql
@@ -0,0 +1,13 @@
+Create table If Not Exists Activities (sell_date date, product varchar(20))
+Truncate table Activities
+insert into Activities (sell_date, product) values ('2020-05-30', 'Headphone')
+insert into Activities (sell_date, product) values ('2020-06-01', 'Pencil')
+insert into Activities (sell_date, product) values ('2020-06-02', 'Mask')
+insert into Activities (sell_date, product) values ('2020-05-30', 'Basketball')
+insert into Activities (sell_date, product) values ('2020-06-01', 'Bible')
+insert into Activities (sell_date, product) values ('2020-06-02', 'Mask')
+insert into Activities (sell_date, product) values ('2020-05-30', 'T-Shirt')
+
+SELECT sell_date, COUNT(DISTINCT(product)) AS num_sold, GROUP_CONCAT(DISTINCT(product)) AS products
+FROM Activities
+GROUP BY sell_date
