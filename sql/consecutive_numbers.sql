86f6aecc0bec83d8715fffe01316dc4151c4fadc
Berat Cankar <berat.cankar@gmail.com>
2023-01-03 19:29:01 +0000

    Show life is better when you have cat and solve sql questions

---
+++ sql/consecutive_numbers.sql
@@ -0,0 +1,14 @@
+Create table If Not Exists Logs (id int, num int)
+Truncate table Logs
+insert into Logs (id, num) values ('1', '1')
+insert into Logs (id, num) values ('2', '1')
+insert into Logs (id, num) values ('3', '1')
+insert into Logs (id, num) values ('4', '2')
+insert into Logs (id, num) values ('5', '1')
+insert into Logs (id, num) values ('6', '2')
+insert into Logs (id, num) values ('7', '2')
+
+
+SELECT DISTINCT(num) AS ConsecutiveNums
+FROM Logs
+WHERE (id + 1, num) IN (SELECT * FROM Logs) AND (id + 2, num) IN (SELECT * FROM Logs)
---
+++ sql/deparment_highest_salary.sql
@@ -0,0 +1,16 @@
+Create table If Not Exists Employee (id int, name varchar(255), salary int, departmentId int)
+Create table If Not Exists Department (id int, name varchar(255))
+Truncate table Employee
+insert into Employee (id, name, salary, departmentId) values ('1', 'Joe', '70000', '1')
+insert into Employee (id, name, salary, departmentId) values ('2', 'Jim', '90000', '1')
+insert into Employee (id, name, salary, departmentId) values ('3', 'Henry', '80000', '2')
+insert into Employee (id, name, salary, departmentId) values ('4', 'Sam', '60000', '2')
+insert into Employee (id, name, salary, departmentId) values ('5', 'Max', '90000', '1')
+Truncate table Department
+insert into Department (id, name) values ('1', 'IT')
+insert into Department (id, name) values ('2', 'Sales')
+
+SELECT d.name AS Department, e.name AS Employee, e.salary AS Salary
+FROM Employee e
+INNER JOIN Department d ON e.departmentId = d.id
+WHERE (d.id, e.salary) IN (SELECT emp.departmentId, MAX(emp.salary) FROM Employee emp GROUP BY 1)
